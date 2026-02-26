ead32b21e2bf4f3a7570e5d8b8dbd2e05a0eefaf
Chandni Raikengar <chandnirai.cs@gmail.com>
2026-02-26 11:23:53 +0500

    Initial Commit

---
+++ src/main/java/com/hospitalMngmntSystem/hospitalMngmntSystem/HospitalMngmntSystemApplication.java
@@ -0,0 +1,13 @@
+package com.hospitalMngmntSystem.hospitalMngmntSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalMngmntSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalMngmntSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hospitalMngmntSystem/hospitalMngmntSystem/HospitalMngmntSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospitalMngmntSystem.hospitalMngmntSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalMngmntSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
